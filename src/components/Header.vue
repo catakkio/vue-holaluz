6ac02e297e55f656f0fa0a8e27530917d5fb824f
Alessandro Catacchio <a.catacchio5@gmail.com>
2022-09-22 10:34:49 +0200

    Tailwind, scss imported

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header class="flex justify-center px-0 py-1">
+      <img src="../assets/logo.png" alt="holaluz logo" class="logo" />
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style scoped>
+</style>
